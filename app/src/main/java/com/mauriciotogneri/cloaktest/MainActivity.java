23be0d36a712b6eeaa7e6eec65a2f98b671ede90
mtogneri <mtogneri@openwt.com>
2017-03-14 22:19:19 +0100

    Added tests for encrypted preferences

--- app/src/main/java/com/mauriciotogneri/cloaktest/MainActivity.java
+++ app/src/main/java/com/mauriciotogneri/cloaktest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mauriciotogneri.cloaktest;
 
 import android.app.Activity;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,8 +10,14 @@ import android.widget.Toast;
 
 import com.mauriciotogneri.cloack.Cloak;
 import com.mauriciotogneri.cloack.CloakKey;
+import com.mauriciotogneri.cloack.EncryptedPreferences;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends Activity
 {
@@ -41,6 +48,7 @@ public class MainActivity extends Activity
             {
                 runTest1(cloak);
                 runTest2(cloak);
+                runTest3();
             }
             catch (Exception e)
             {
@@ -100,6 +108,99 @@ public class MainActivity extends Activity
             }
 
             log("TEST " + i + "B: OK");
+        }
+    }
+
+    private void runTest3() throws Exception
+    {
+        String stringValue = "Peter";
+        int intValue = 123;
+        float floatValue = 123.456f;
+        long longValue = 1234567;
+        boolean booleanValue = (System.currentTimeMillis() % 2 == 0);
+        Set<String> setValue = new HashSet<>();
+        setValue.add("A");
+        setValue.add("B");
+        setValue.add("C");
+
+        EncryptedPreferences preferences = new EncryptedPreferences(this, "my_preferences", true);
+        Editor editor = preferences.edit();
+        editor.clear();
+        editor.putString("stringValue", stringValue);
+        editor.putInt("intValue", intValue);
+        editor.putFloat("floatValue", floatValue);
+        editor.putLong("longValue", longValue);
+        editor.putBoolean("booleanValue", booleanValue);
+        editor.putStringSet("setValue", setValue);
+        editor.commit();
+
+        String loadedStringValue = preferences.getString("stringValue", null);
+        if (stringValue.equals(loadedStringValue))
+        {
+            Log.d("TEST", loadedStringValue);
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+
+        int loadedInt = preferences.getInt("intValue", 0);
+        if (intValue == loadedInt)
+        {
+            Log.d("TEST", String.valueOf(loadedInt));
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+
+        float loadedFloat = preferences.getFloat("floatValue", 0);
+        if (floatValue == loadedFloat)
+        {
+            Log.d("TEST", String.valueOf(loadedFloat));
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+
+        long loadedLong = preferences.getLong("longValue", 0);
+        if (longValue == loadedLong)
+        {
+            Log.d("TEST", String.valueOf(loadedLong));
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+
+        boolean loadedBoolean = preferences.getBoolean("booleanValue", false);
+        if (booleanValue == loadedBoolean)
+        {
+            Log.d("TEST", String.valueOf(loadedBoolean));
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+
+        Set<String> loadedSet = preferences.getStringSet("setValue", null);
+        if (setValue.equals(loadedSet))
+        {
+            Log.d("TEST", Arrays.toString(loadedSet.toArray()));
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+
+        File file = new File("/data/data/com.mauriciotogneri.cloaktest/shared_prefs/my_preferences.xml");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+
+        while ((st = br.readLine()) != null)
+        {
+            Log.d("TEST", st);
         }
     }
 
